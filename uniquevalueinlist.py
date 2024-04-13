e7c387012ab2021d77a8400d0080d0b04c48e73d
Thi Nguyen <46949699+ClupusVT@users.noreply.github.com>
2024-04-14 00:27:19 +0700

    Update uniquevalueinlist.py

--- uniquevalueinlist.py
+++ uniquevalueinlist.py
@@ -6,8 +6,18 @@ def get_unique_values(lst):
     
     return list1
 
-            
+
+def is_prime(n):
+    count = 0
+    for i in range(1, n + 1):
+        if n % i == 0:
+            count += 1
+    if count == 2:
+        return True
+    return False
 
 
+n = int(input())
+print(is_prime(n))
 
 lst = [4,5,6,7,8,2]
